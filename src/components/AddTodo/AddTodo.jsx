5bbfc3e731546d3eb02f48342110bb32abd1c905
VladRastortguev <128030167+VladRastortguev@users.noreply.github.com>
2023-05-22 10:32:10 +0600

    add todo

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <div>
+        <h2>Add TODO component</h2>
+        <input type="text" />
+        <button>add</button>
+    </div>
+  )
+}
+
+export default AddTodo
